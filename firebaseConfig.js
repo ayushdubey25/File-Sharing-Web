d575b43e0d68c73104a5282ea17629362403a065
Ayush Dubey <154453637+ayushdubey25@users.noreply.github.com>
2024-09-04 20:49:51 +0530

    firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAQ8cCxQMelMl2FwvSEQ6D9z85YEl6Kl6Q",
-  authDomain: "mysync-file-sharing-web.firebaseapp.com",
-  projectId: "mysync-file-sharing-web",
-  storageBucket: "mysync-file-sharing-web.appspot.com",
-  messagingSenderId: "1046356993972",
-  appId: "1:1046356993972:web:604e83b2846767421f74e1",
-  measurementId: "G-BWBFJSSHT0"
+  apiKey: "*******************",
+  authDomain: "*******************",
+  projectId: "*******************b",
+  storageBucket: "*******************",
+  messagingSenderId: "*******************",
+  appId: "*******************1",
+  measurementId: "*******************"
 };
 
 // Initialize Firebase
